c0060c7dfda32d44855f7e90013bfb3a856fc481
Saif <saif@Arifs-MBP-4.lan>
2023-09-28 16:05:32 -0400

    Argument Destructuring

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,8 +1,14 @@
-function ProfileCard(props) {
+function ProfileCard({title,handle}) {
+    // const title = props.title;
+    // const handle = props.handle;
+
+    // This is the same as the 2 lines of code above
+    // const {title,handle} = props;
+
     return (
         <div>
-            <div>Title is {props.title}</div>
-            <div>Handle is {props.handle}</div>
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
         </div>
     );
 }
