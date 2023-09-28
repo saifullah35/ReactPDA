ee9ad89d8cb1f463069a1b00f675ddba44400c00
Saif <saif@Arifs-MBP-4.lan>
2023-09-28 15:59:32 -0400

    Added Props

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import ProfileCard from "./ProfileCard";
 function App() {
     return(
         <div>
-            <div>Personal Digital Assistant</div>
+            <div>Personal Digital Assistants</div>
 
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
+            {/* Attribute names can be anything user prefers */}
+            <ProfileCard title= "Alexa" handle = "@alexa99" />
+            <ProfileCard title= "Cortana" handle= "@coratana32" />
+            <ProfileCard title= "Siri" handle= "@siri01" />
         </div>
     );
 }
