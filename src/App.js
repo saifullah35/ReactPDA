9cb86f9ae454ebf6e4264e72398990a20ec59809
Saif <saif@Arifs-MBP-4.lan>
2023-10-05 13:19:56 -0400

    Completed Section 3: Building with reusable Components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from "./ProfileCard";
 import AlexaImage from './images/alexa.png';
 import CortanaImage from './images/cortana.png';
 import SiriImage from './images/siri.png';
 
-
 function App() {
     return(
         <div>
-            <div>Personal Digital Assistants</div>
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">Personal Digital Assistants</p>
+                </div>
+            </section>
 
-            {/* Attribute names can be anything user prefers */}
-            <ProfileCard title= "Alexa" handle = "@alexa99" image={AlexaImage} />
-            <ProfileCard title= "Cortana" handle= "@coratana32" image={CortanaImage} />
-            <ProfileCard title= "Siri" handle= "@siri01" image={SiriImage} />
+             {/* Attribute names can be anything user prefers */}
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard 
+                            title= "Alexa" 
+                            handle = "@alexa99" 
+                            image={AlexaImage} 
+                            content="Alexa was created by Amazon and helps you buy things."
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard 
+                            title= "Cortana" 
+                            handle= "@coratana32" 
+                            image={CortanaImage} 
+                            content="Cortana was created by Microsoft."
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard 
+                            title= "Siri" 
+                            handle= "@siri01" 
+                            image={SiriImage} 
+                            content="Siri was created by Apple and fazed out."
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,4 +1,4 @@
-function ProfileCard({title, handle, image}) {
+function ProfileCard({title, handle, image, content}) {
     
     // const title = props.title;
     // const handle = props.handle;
@@ -10,10 +10,20 @@ function ProfileCard({title, handle, image}) {
     // since we don't use the props object throughout the whole project
 
     return (
-        <div>
-            <img src = {image} alt = "pda logo"/>
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                <img src = {image} alt = "pda logo"/>
+                </figure>
+            </div>
+
+            <div className ="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{handle}</p>
+                </div>
+                <div className="content">{content}</div>
+            </div>
         </div>
     );
 }
