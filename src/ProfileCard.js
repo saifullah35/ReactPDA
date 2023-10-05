abfece519272e5d0e74502230ed72166a0258ccc
Saif <saif@Arifs-MBP-4.lan>
2023-10-05 12:06:06 -0400

    added images folder

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,9 +1,13 @@
 function ProfileCard({title,handle}) {
+    
     // const title = props.title;
     // const handle = props.handle;
 
     // This is the same as the 2 lines of code above
     // const {title,handle} = props;
+
+    // ({title,handle}) is the same as the code above and much simpler to use 
+    // since we don't use the props object throughout the whole project
 
     return (
         <div>
